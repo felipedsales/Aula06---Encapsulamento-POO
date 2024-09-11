f48d4e2a4cebccd2a715b9a80e978686fad7930a
Felipe de Sales <71357928+felipedsales@users.noreply.github.com>
2024-09-11 12:37:56 -0300

    Add arquivos de encapsulamento

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Aluno chico = new Aluno("Felipe",29999,25,"Masculino",true);
+        //chico.setNome("Felipe");
+        System.out.println(chico.getNome());
+    }
+}
