034b6a0651eea0941875f688d7d26cb8d13248d0
felipe <felipe_ssales@hotmail.com>
2024-09-25 20:38:03 -0300

    Adicionando pacotes JAVA

--- App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-
-        Aluno chico = new Aluno("Felipe",29999,25,"Masculino",true);
-        //chico.setNome("Felipe");
-        System.out.println(chico.getNome());
-    }
-}
